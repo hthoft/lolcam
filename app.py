2369f01614768377e2ef7fe279c2d96fe00b43e5
Hans Thoft Rasmussen <72069575+hthoft@users.noreply.github.com>
2024-02-05 09:02:04 +0100

    Testing Camera

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from flask import Flask, redirect, request, render_template, jsonify
-from time import sleep
-
+from flask import Flask, render_template, jsonify, Response
+from picamera2 import PiCamera, PiCameraMk2Renderer
+import time
 
 app = Flask(__name__)
 
+# Initialize the Pi Camera
+camera = PiCamera()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route("/initate", methods=['POST', 'GET'])
-def initate():
-    print("Starter cam")
+@app.route("/initiate", methods=['POST', 'GET'])
+def initiate():
+    print("Starting cam")
     sleep(5)
     return jsonify({'hide': True})
 
+# Stream the camera feed
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generate_frames():
+    renderer = PiCameraMk2Renderer(camera)
+    camera.start_recording('/dev/null', format='h264', resize=(640, 480))
+    camera.start_recording(renderer, format='mjpeg', resize=(640, 480))
+
+    try:
+        while True:
+            camera.wait_recording(1)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + renderer.frame() + b'\r\n')
+    finally:
+        camera.stop_recording()
+
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
-    try: 
+    try:
         app.run(host='0.0.0.0', port=50005, debug=True, use_reloader=False)
     finally:
         pass
