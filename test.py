75d6fa3a151517d60c051986e0bdcceaed7e916c
Ramlov <mathias.ramlov10@gmail.com>
2024-02-08 19:02:29 +0100

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 
+from drive_uploader import upload_picture
+from drive_folder import create_folder_in_drive
 
+folder_id = create_folder_in_drive()
+filename =  "IMG_1935.JPEG"
 
+upload_picture(filename, folder_id)
