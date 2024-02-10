94a05632b5d604ab4a3dba42af92794e3d01a8f8
Ramlov <mathias.ramlov10@gmail.com>
2024-02-10 11:28:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from drive_uploader import upload_picture
 from drive_folder import create_folder_in_drive
 
 folder_id = create_folder_in_drive()
-filename =  "IMG_1935.JPEG"
+filename =  "favicon.ico"
 upload_picture(filename, folder_id)
 
 url = "https://drive.google.com/drive/folders/"
